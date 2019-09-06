63506bf3dc0115702448a969e8767b6e3294739d
Ajay Thankiah S P <aj@Ajays-MBP.attlocal.net>
2019-09-05 21:06:16 -0400

    Initial setup with mongo db config

---
+++ src/main/java/com/fitness/manager/fitnessanalyzer/FitnessAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.manager.fitnessanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness/manager/fitnessanalyzer/FitnessAnalyzerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fitness.manager.fitnessanalyzer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FitnessAnalyzerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
